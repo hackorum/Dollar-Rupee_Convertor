da467b480c0f42256d95ad6bd14e8b3540158ebb
hackorum <neilsabde@gmsil.com>
2020-09-28 11:53:16 +0530

    First Commit

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var input, button, dollartorupee;
+
+function setup() {
+   createCanvas(710, 400);
+
+   input = createInput();
+   input.position(20, 65);
+
+   button = createButton('convert');
+   button.position(input.x + input.width, 65);
+   button.mousePressed(DollarInRupee);
+
+   greeting = createElement('h2', 'Enter amount in dollars $:');
+   greeting.position(20, 5);
+
+   textAlign(CENTER);
+   textSize(50);
+}
+
+async function DollarInRupee() {
+  const dollar = input.value();
+  var response = await fetch("https://api.exchangeratesapi.io/latest?base=USD");
+  var responseJSON = await response.json();
+  var rates = responseJSON.rates;
+  var inr = rates.INR;
+
+  greeting.html('Amount in ₹upees: ' + dollar*inr);
+  input.value('');
+
+}
